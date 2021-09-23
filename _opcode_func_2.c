5fc709ab0d0fb6241af41c1898cf6fe8dce9ca34
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-09-23 16:40:45 -0700

    div

--- _opcode_func_2.c
+++ _opcode_func_2.c
@@ -60,3 +60,35 @@ void _op_sub(stack_t **head, unsigned int line_number)
 	(*head)->prev = NULL;
 	free(temp);
 }
+
+
+/**
+ * _op_div - divides the second top element
+ * of the stack by the top element of the stack.
+ * @head: head (stack) to the stack
+ * @line_number: line number where opcode is located
+ */
+void _op_div(stack_t **head, unsigned int line_number)
+{
+        stack_t *temp;
+
+        if (*head == NULL || (*head)->next == NULL)
+        {
+                fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+                value[1] = -1;
+                return;
+        }
+
+	if ((*head)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		value[1] = -1;
+		return;
+	}
+        temp = *head;
+        (*head)->next->n /= (*head)->n;
+        *head = (*head)->next;
+        (*head)->prev = NULL;
+        free(temp);
+
+}
--- monty.h
+++ monty.h
@@ -78,4 +78,6 @@ void _op_swap(stack_t **head, unsigned int line_number);
 void _op_add(stack_t **head, unsigned int line_number);
 void _op_nop(stack_t **head, unsigned int line_number);
 void _op_sub(stack_t **head, unsigned int line_number);
+void _op_div(stack_t **head, unsigned int line_number);
+
 #endif
